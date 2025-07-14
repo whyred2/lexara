7a8108ccb5da61a99dcb40f2eec5692fbcce6f63
Dmitry <dima74181@gmail.com>
2025-07-14 11:12:26 +0300

    fix: input props supertype

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -3,15 +3,18 @@ import * as React from "react";
 import { cn } from "@/lib/utils";
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  isError?: boolean;
+}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
+  ({ className, type, isError, ...props }, ref) => {
     return (
       <input
         type={type}
         className={cn(
           "rounded-full border border-white/5 bg-white/2.5 px-4 py-2 text-lg",
+          isError && "border-red-500",
           className,
         )}
         ref={ref}
