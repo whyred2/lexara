51316cda731125fe0f84880d6837ccba885f75f8
Dmitry <dima74181@gmail.com>
2025-07-19 17:21:47 +0300

    fix: added esling ignor for prisma

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,6 +10,9 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  {
+    ignores: ["lib/generated/prisma**"],
+  },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
