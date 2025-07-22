b8aaff90d7adeb8e546797d0bf3d110d870689cc
Dmitry <dima74181@gmail.com>
2025-07-22 20:28:55 +0300

    fix: added types for svg icons

--- next.config.ts
+++ next.config.ts
@@ -15,7 +15,7 @@ const nextConfig: NextConfig = {
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/,
-      use: ["@svgr/webpack", "url-loader"],
+      use: ["@svgr/webpack"],
     });
 
     return config;
