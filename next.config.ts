021724854de04437117c02bb2fe1cfd760bcd852
Dmitry <dima74181@gmail.com>
2025-07-22 20:45:15 +0300

    fix: next config

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,15 @@ import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
-  turbopack: {
-    rules: {
-      "*svg": {
-        loaders: ["@svgr/webpack"],
-        as: "*.js",
-      },
-    },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
   },
+  reactStrictMode: true,
 };
 
 const withNextIntl = createNextIntlPlugin();
