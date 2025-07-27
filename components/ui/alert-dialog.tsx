1c5d0269e13376f007aa6b459b59425cf6bbb2bc
Dmitry <dima74181@gmail.com>
2025-07-27 17:17:23 +0300

    fix: ui dialog className

--- components/ui/alert-dialog.tsx
+++ components/ui/alert-dialog.tsx
@@ -14,9 +14,16 @@ const AlertDialogPortal = ({
   className,
   children,
   ...props
-}: AlertDialogPrimitive.AlertDialogPortalProps) => (
-  <AlertDialogPrimitive.Portal className={cn(className)} {...props}>
-    <div className="fixed inset-0 z-50 flex items-end justify-center sm:items-center">
+}: AlertDialogPrimitive.AlertDialogPortalProps & {
+  className?: string;
+}) => (
+  <AlertDialogPrimitive.Portal {...props}>
+    <div
+      className={cn(
+        "fixed inset-0 z-50 flex items-end justify-center sm:items-center",
+        className,
+      )}
+    >
       {children}
     </div>
   </AlertDialogPrimitive.Portal>
